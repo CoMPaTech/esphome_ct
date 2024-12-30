30dee55dfd7304b1d3ee0bd6a36f4ba96714ad63
Tom Scholten <git@scholten.nu>
2024-12-30 12:27:52 +0100

    Debugging - readd listening

--- components/cc1101/fan.h
+++ components/cc1101/fan.h
@@ -1,7 +1,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "esphome/components/fan/fan.h"
-#include "esphome/components/gpio/gpio.h"
+#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace cc1101fan {
