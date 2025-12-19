fb37214d98a2cf466d6abc2e5a4ee7f6d4ad7d3a
Tom Scholten <git@scholten.nu>
2025-12-19 11:03:59 +0100

    Minimal viable

---
+++ components/cc1101_ithofan/__init__.py
@@ -0,0 +1,23 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID, CONF_CS_PIN, CONF_GDO0_PIN, CONF_FREQUENCY, CONF_PACKET_LENGTH
+
+cc1101_ns = cg.esphome_ns.namespace("itho_cc1101")
+IthoCC1101 = cc1101_ns.class_("IthoCC1101", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(IthoCC1101),
+    cv.Required(CONF_CS_PIN): cv.pin,
+    cv.Required(CONF_GDO0_PIN): cv.pin,
+    cv.Required(CONF_FREQUENCY): cv.frequency,
+    cv.Required(CONF_PACKET_LENGTH): cv.int_range(min=1, max=64),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    cg.add(var.set_cs_pin(config[CONF_CS_PIN]))
+    cg.add(var.set_gdo0_pin(config[CONF_GDO0_PIN]))
+    cg.add(var.set_frequency(config[CONF_FREQUENCY]))
+    cg.add(var.set_packet_length(config[CONF_PACKET_LENGTH]))
+
